cc4b52c12af04fb5b46dec090fde9ef20a8b8320
Peter-Gichuki <106585269+Peter-Gichuki@users.noreply.github.com>
2023-01-28 14:50:54 +0300

    Add files via upload

---
+++ gross_pay.py
@@ -0,0 +1,5 @@
+#Program to calculate the Gross_Pay of an employee
+hours_worked=int(input("Enter the number of hours worked by the employee="))
+rate_per_hour=float(input("Input the rate of payment per hour="))
+gross_pay= hours_worked*rate_per_hour
+print("The employees' gross pay is:$" ,gross_pay)
---
+++ sphere_volume.py
@@ -0,0 +1,5 @@
+#Program to calculate the volume of a sphere
+pie=22/7
+radius=int(input("Enter the radius of the sphere in meters="))
+volume=4/3*pie*radius*radius
+print("The volume of the sphere of radius",radius, "meters is:", volume)
